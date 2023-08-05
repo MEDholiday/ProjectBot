0ce26462b241174caea2c9a24030aaff6740875d
MEDholiday <139779331+MEDholiday@users.noreply.github.com>
2023-08-05 15:48:07 +0300

    Update chat_id.py

--- chat_id.py
+++ chat_id.py
@@ -1,11 +1,12 @@
 import requests
-from config import token2
+from config import token
 
 bot_token = "YOUR_BOT_TOKEN"
-response = requests.get(f"https://api.telegram.org/bot{token2}/getUpdates")
+response = requests.get(f"https://api.telegram.org/bot{token}/getUpdates")
 data = response.json()
 chat_id = data["result"][0]["message"]["chat"]["id"]
 
 print("ID чата с ботом:", chat_id)
 
 # 296318553
+# 
