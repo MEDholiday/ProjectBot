4dce2c5fbbdc7097ca44f5a74a317a0258d8f661
MEDholiday <139779331+MEDholiday@users.noreply.github.com>
2023-08-05 19:38:24 +0700

    Add files via upload

--- config.py
+++ config.py
@@ -1,3 +1,7 @@
 api_id = 29437552
 api_hash = '4cec46425dfef20ad7f5f6a07f5d465b'
 token = '6090941045:AAESxJI1KwdNTTvOXJFLBvWBXylWwN6o7ys'
+token2 = '6001813945:AAFwr_rKWHE2zOuiEwDxZZ-Rw5bf_wYTCR4'
+chat_id = '296318553'
+api_id2 = '28082986'
+api_hash2 = 'fafd077d76adc6f1cb4f8384856ac63b'
