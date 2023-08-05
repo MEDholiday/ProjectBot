49c98755d9292023232d619412a5162be2450aac
MEDholiday <139779331+MEDholiday@users.noreply.github.com>
2023-08-05 16:48:36 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,9 @@
 api_id = 29437552
 api_hash = '4cec46425dfef20ad7f5f6a07f5d465b'
 token = '6090941045:AAESxJI1KwdNTTvOXJFLBvWBXylWwN6o7ys'
+chat_id = '233349472'
 token2 = '6001813945:AAFwr_rKWHE2zOuiEwDxZZ-Rw5bf_wYTCR4'
-chat_id = '296318553'
+chat_id2 = '296318553'
 api_id2 = '28082986'
 api_hash2 = 'fafd077d76adc6f1cb4f8384856ac63b'
-chat_id2 = '233349472'
+
