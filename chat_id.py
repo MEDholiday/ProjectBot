8295d4473f0931874f96f9142accbaabb56e1fa1
MEDholiday <139779331+MEDholiday@users.noreply.github.com>
2023-08-05 19:36:16 +0700

    Add files via upload

---
+++ chat_id.py
@@ -0,0 +1,11 @@
+import requests
+from config import token2
+
+bot_token = "YOUR_BOT_TOKEN"
+response = requests.get(f"https://api.telegram.org/bot{token2}/getUpdates")
+data = response.json()
+chat_id = data["result"][0]["message"]["chat"]["id"]
+
+print("ID чата с ботом:", chat_id)
+
+# 296318553
